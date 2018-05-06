d4c47a0529fbf462879835016caf7e3e6cdb0030
Naes0 <seanoneill8@gmail.com>
2018-05-06 17:53:53 +0800

    somewhat working

--- Threads/sds.c
+++ Threads/sds.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/syscall.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <pthread.h>
 #include "sds.h"
 
-
-Data* data;
-pthread_mutex_t mutex;
-pthread_cond_t writer;
-pthread_cond_t reader;
-
+Data data;
 int r;
 int w;
 int timer1;
 int timer2;
 
+pthread_mutex_t mutex;
+pthread_cond_t writert;
+pthread_cond_t readert;
+pthread_cond_t sharedData;
+
+
 int main(int argc, char const *argv[])
 {
-
    int i = 0;
-   r = 0
+   r = 0;
    w = 0;
    timer1 = 0;
    timer2 = 0;
@@ -43,56 +44,57 @@ int main(int argc, char const *argv[])
       printf("r = %d, w = %d, t1 = %d, t2 = %d\n", r, w, timer1, timer2);
 
       //initialise Data
-      data->readercount = 0;
-      data->writercount = 0;
-      data->writerDataIndex = 0;
-      data->dataBufferIndex = 0;
-      data->readersRun = r;
+      data.readercount = 0;
+      data.writercount = 0;
+      data.writerDataIndex = 0;
+      data.dataBufferIndex = 0;
+      data.readersRun = r;
 
       //initialising data buffer to zero
       for(int i = 0; i < 20; i++)
       {
-         data->dataBuffer[i] = 0;
+         data.dataBuffer[i] = 0;
       }
 
       // reading from shared_data into array
-      data->fp = fopen("shared_data", "r");
-      if (data->fp == NULL)
+      data.fp = fopen("shared_data", "r");
+      if (data.fp == NULL)
       {
          printf("Error: Could not open file\n");
       }
       else
       {
-         while (feof(data->fp) == 0)
+         while (feof(data.fp) == 0)
          {
-            fscanf(data->fp, "%d,", &data->sharedData[i]);
+            fscanf(data.fp, "%d,", &data.sharedData[i]);
             i++;
          }
-         if(ferror(data->fp))
+         if(ferror(data.fp))
          {
             perror("Error while reading.");
          }
-         fclose(data->fp);
+         fclose(data.fp);
       }
       //opening file writer to write to the file sim_out
-      data->fp = fopen("sim_out", "w");
+      data.fp = fopen("sim_out", "w");
 
       pthread_mutex_init(&mutex, NULL);
-      pthread_cond_init(&writer, NULL);
-      pthread_cond_init(&reader, NULL);
+      pthread_cond_init(&writert, NULL);
+      pthread_cond_init(&readert, NULL);
+      pthread_cond_init(&sharedData, NULL);
 
       produceThreads();
-
    }
-   fclose(data->fp); //closing file pointer
+   fclose(data.fp); //closing file pointer
 
    // final print of the reader and writer counts
-   printf("\nreadcount: %d\nwritecount: %d\n", data->readercount, data->writercount);
+   printf("\nreadcount: %d\nwritecount: %d\n", data.readercount, data.writercount);
 
    //destory threads;
    pthread_mutex_destroy(&mutex);
-   pthread_cond_destroy(&writer);
-   pthread_cond_destroy(&reader);
+   pthread_cond_destroy(&writert);
+   pthread_cond_destroy(&readert);
+   pthread_cond_destroy(&sharedData);
 
    return 0;
 }
@@ -116,106 +118,109 @@ void produceThreads()
    //joining
    for (int i = 0; i < r; i++)
    {
-      pthread_join(&rthreads[i], NULL);
+      pthread_join(rthreads[i], NULL);
    }
    for (int i = 0; i < w; i++)
    {
-      pthread_join(&wthreads[i], NULL);
+      pthread_join(wthreads[i], NULL);
    }
 }
-
 
 void* reader(void* nothing)
 {
    int readSize = 0; //amount of data read
    int readData = 0; //read data cant be zero so this can be intiialised
 
-   int value = 0;
-   printf("\nI am reader: %d", (int) getpid());
+   printf("\nI am reader: %d", (int)pthread_self());
+
+   pthread_mutex_lock(&mutex);
 
    while(readSize != 100) // while each reader process has not read 100 times
    {
-      sem_wait(&sems->semReader); //lock reader 0
-         data->readercount++;
-         if(data->readercount == 1)
+      pthread_cond_wait(&readert, &mutex); //lock reader 0
+         data.readercount++;
+         if(data.readercount == 1)
          {
-            sem_wait(&sems->semWriter); //lock writer 0
+            pthread_cond_wait(&writert, &mutex); //lock writer 0
          }
-      sem_post(&sems->semReader); //unlock reader 1
+      pthread_cond_signal(&readert); //unlock reader 1
 
          //reading is preformed
-         if(data->dataBufferIndex == 20) //if the writers have filled the data buffer
+         if(data.dataBufferIndex == 20) //if the writers have filled the data buffer
          {
-            if (data->readersRun == r) // if the current reader processes running equal the total readers created
+            if (data.readersRun == r) // if the current reader processes running equal the total readers created
             {
                for(int i = 0; i < 20; i++) // then read from data buffer
                {
-                  readData = data->dataBuffer[i];
-                  printf("\nReader: %d    Read Data: %d\n", getpid(), readData); //print read data
+                  readData = data.dataBuffer[i];
+                  printf("\nReader: %d    Read Data: %d\n", (int)pthread_self(), readData); //print read data
                   readSize++;
                   sleep(timer1);
                }
-               sem_wait(&sems->semDBuffer);
-                  data->readersRun--; // decrease running readers since reading has finished
-               sem_post(&sems->semDBuffer);
+               data.readersRun--; // decrease running readers since reading has finished
             }
          }
          //end reading
 
          //reseting buffer
-         sem_wait(&sems->semDBuffer);
-         if(data->readersRun == 0)
+         if(data.readersRun == 0)
          {
             for (int i = 0; i < 20; i++)
             {
-               data->dataBuffer[i] = 0; //reset buffer to 0;
+               data.dataBuffer[i] = 0; //reset buffer to 0;
             }
-            data->readersRun = r;// sets running readers back to enter value for the next set of 20 elements
-            data->dataBufferIndex = 0; //setting index back to 0 so the writer starts from element 0 when writing to buffer
+            data.readersRun = r;// sets running readers back to enter value for the next set of 20 elements
+            data.dataBufferIndex = 0; //setting index back to 0 so the writer starts from element 0 when writing to buffer
          }
-         sem_post(&sems->semDBuffer);
 
-
-      sem_wait(&sems->semReader); //locks reader 0
-         data->readercount--;
-         if(data->readercount == 0)
+      pthread_cond_wait(&readert, &mutex); //locks reader 0
+         data.readercount--;
+         if(data.readercount == 0)
          {
-            sem_post(&sems->semWriter); //unlock writer 1
+            pthread_cond_signal(&writert); //unlock writer 1
          }
-         sem_post(&sems->semReader); //unlock reader 1
+         pthread_cond_signal(&readert); //unlock reader 1
    }
    printf("\nreadSize: %d\n", readSize);
-   sem_wait(&sems->semSharedData);
-      fprintf(data->fp, "\nReader-%d has finished reading %d pieces of data from the data_buffer\n", getpid(), readSize); //printing out to sim_out
-   sem_post(&sems->semSharedData);
-   exit(0); //terminate process
+
+   pthread_cond_wait(&sharedData, &mutex);
+      fprintf(data.fp, "\nReader-%d has finished reading %d pieces of data from the data_buffer\n", (int)pthread_self(), readSize); //printing out to sim_out
+   pthread_cond_signal(&sharedData);
+
+   pthread_mutex_unlock(&mutex);
+
+   pthread_exit(NULL); //terminate thread
 }
 
 void* writer(void* nothing)
 {
    int writeSize = 0; //amount of data written
    int writeData = 0; //read data cant be zero so this can be intiialised
+   printf("\nI am writer: %d", (int) (int)pthread_self());
+   pthread_mutex_lock(&mutex);
 
-   sem_wait(&sems->semWriter);
-   while(data->writerDataIndex != 100)
+   pthread_cond_wait(&writert, &mutex);
+   while(data.writerDataIndex != 100)
    {
-      sem_post(&sems->semWriter);
-            sem_wait(&sems->semWriter );
-               if(data->dataBufferIndex < 20) //buffer is no full
+      pthread_cond_signal(&writert);
+            pthread_cond_wait(&writert, &mutex);
+               if(data.dataBufferIndex < 20) //buffer is no full
                {
-                  writeData = data->sharedData[data->writerDataIndex]; //get data from shared data
-                  data->dataBuffer[data->dataBufferIndex] = writeData; //write the recieved data to the data buffer
-                  printf("\nWriter: %d    Written Data: %d    DataBuffer[%d]: %d \n", getpid(), writeData, data->dataBufferIndex, data->dataBuffer[data->dataBufferIndex]);   //print write data
-                  data->writercount++; //increment counters
-                  data->writerDataIndex++;
-                  data->dataBufferIndex++;
+                  writeData = data.sharedData[data.writerDataIndex]; //get data from shared data
+                  data.dataBuffer[data.dataBufferIndex] = writeData; //write the recieved data to the data buffer
+                  printf("\nWriter: %d    Written Data: %d    DataBuffer[%d]: %d \n", (int)pthread_self(), writeData, data.dataBufferIndex, data.dataBuffer[data.dataBufferIndex]);   //print write data
+                  data.writercount++; //increment counters
+                  data.writerDataIndex++;
+                  data.dataBufferIndex++;
                   writeSize++;
                   sleep(timer2);
                }
-            sem_post(&sems->semWriter);
+            pthread_cond_signal(&writert);
    }
-   sem_wait(&sems->semSharedData);
-      fprintf(data->fp, "\nWriter-%d has finished writing %d pieces of data to the data_buffer\n", getpid(), writeSize); //print out to sim_out
-   sem_post(&sems->semSharedData);
-   exit(0);
+   pthread_cond_wait(&sharedData, &mutex);
+      fprintf(data.fp, "\nWriter-%d has finished writing %d pieces of data to the data_buffer\n", (int)pthread_self(), writeSize); //print out to sim_out
+   pthread_cond_signal(&sharedData);
+   pthread_mutex_unlock(&mutex);
+
+   pthread_exit(NULL); //terminate thread
 }
